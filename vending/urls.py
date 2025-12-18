08b71b0ed9339b10c30cd8c9a103a3ba1ce1479c
atefhamdy05 <atefhamdy05@gmail.com>
2025-12-18 22:58:45 +0200

    add transaction list end point

--- vending/urls.py
+++ vending/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import  Buy, Reset,Deposit
+from .views import  Buy, Reset,Deposit, transactions_list
 
 urlpatterns = [
     path('deposit/', Deposit.as_view()),
     path('buy/', Buy.as_view()),
     path('reset/', Reset.as_view()),
+    path('transactions_list/',transactions_list,name="transaction_list"),
+
 ]
